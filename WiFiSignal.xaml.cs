579f0003e736c022d6b18fc6fea196942c21e745
XBold <80431693+XBold@users.noreply.github.com>
2025-03-03 14:04:21 +0100

    - Prepared first attempt of WiFi signal page - Slider to change the update frequency of the wifi signal - Label with details about WiFi signal and IP

--- WiFiSignal.xaml.cs
+++ WiFiSignal.xaml.cs
@@ -1,9 +1,36 @@
+using MAUI_Tools;
+using Math = Tools.Math;
 namespace DiagnosticApp;
 
 public partial class WiFiSignal : ContentPage
 {
+    int minFrequency = 100;
+    int maxFrequency = 2000;
 	public WiFiSignal()
 	{
 		InitializeComponent();
-	}
+        sldUpdateFrequency.Minimum = minFrequency;
+        sldUpdateFrequency.Maximum = maxFrequency;
+        sldUpdateFrequency.Value = minFrequency;
+        SldUpdate(sldUpdateFrequency, new ValueChangedEventArgs(0, minFrequency));  //Initial startup
+        _ = UpdateWifiData();
+    }
+
+    private async Task UpdateWifiData()
+    {
+        int frequency;
+        while (true)
+        {
+            frequency = Math.Limit(min: minFrequency, value: (int)sldUpdateFrequency.Value, max: maxFrequency);
+            var (signal, ip) = WiFiHelper.GetWiFiData();
+            lblWiFiSignal.Text = $"IP: {ip}\nSignal: {signal.ToString()}";
+            await Task.Delay(frequency);
+        }
+    }
+
+    private void SldUpdate(object sender, ValueChangedEventArgs e)
+    {
+        int frequency = (int)e.NewValue;
+        lblFrequency.Text = $"Frequency: {frequency} ms";
+    }
 }
